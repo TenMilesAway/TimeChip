0df32512f833166c5c07b475a60e4cf80cac32d2
TenMilesAway <zll749486310@163.com>
2026-09-17 16:24:52 +0800

    【UI】钓鱼商店

---
+++ Assets/Scripts/Community/FishStoreItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishStoreItem : MonoBehaviour
+{
+    [SerializeField] private Image _imgIcon;
+    [SerializeField] private Text _txtName;
+    [SerializeField] private Text _txtPrice;
+    [SerializeField] private Text _txtCharge;
+    [SerializeField] private Button _btnCharge;
+}
